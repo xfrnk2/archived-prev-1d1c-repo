5a55f30b80439f9fe280aed1ffe5831fa99b34dc
zrma <bulbitain@gmail.com>
2017-12-25 01:32:41 +0900

    1주차 과제 코드 업로드

---
+++ quiz/week1/quiz3.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+
+"""
+아래의 함수를 디버깅 해서 step into 기능을 통해 흐름을 파악해 봅니다.
+아래의 흐름이 어떻게 이루어지는지 설명할 수 있어야 합니다.
+
+"""
+
+
+def fibonacci( n: int ):
+    assert n >= 0, "Invalid argument, must n >= 0"
+    escape_condition = (n is 0) or (n is 1)
+
+    if escape_condition:
+        return 1
+
+    return fibonacci( n - 1 ) + fibonacci( n - 2 )
+
+
+"""
+아래의 함수는 잘못 된 부분이 있습니다.
+적절히 수정해 봅시다.
+"""
+
+
+def factorial( n: int ):
+    if n == 0:
+        return 0
+
+    assert n < 0, "Invalid argument, must n > 0"
+
+    escape_condition = (n == 1)
+    if escape_condition is True:
+        return 1
+
+    return n * factorial( n - 1 )
+
+
+if __name__ == '__main__':
+    x = 10
+    result = fibonacci( x )
+    print( result )
+
+    x = 5
+    result = factorial( x )
+    print( result )
---
+++ quiz/week1/quiz4.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+"""
+하노이의 탑
+- https://namu.wiki/w/하노이의%20탑
+
+하노이의 탑을 구현해 봅시다.
+
+이런, 프로그램을 작성하다 중간에 멈췄네요.
+적당히 완성시켜 봅시다.
+
+"""
+
+
+def hanoi( first: int, second: int, third: int, amount: int ):
+    tags = [ "첫번째", "두번째", "세번째" ]
+
+    # 종료 조건
+    # TODO - 종료 조건을 적당히 수정해 주세요
+    if amount <= ? :
+        # 가장 아래의 것(=amount)이 1개 남았을 경우, 현재 위치에서 목표 위치로 옮기고 종료
+        print( f"원반 {amount}번을 {tags[first]} 고리에서 {tags[third]} 고리로 옮깁니다." )
+
+    else:
+        # 가장 아래의 것(=amount)의 바로 위 전체(amount-1부터 1까지)를 목표의 옆으로 옮기고
+        hanoi( first, third, second, amount - 1 )
+
+        # 가장 아래의 것(=amount)를 목표로 옮기고
+        print( f"원반 {amount}번을 {tags[first]} 고리에서 {tags[third]} 고리로 옮깁니다." )
+
+        # 아까 목표 옆에 치워뒀던 나머지(amount-1부터 1까지)를 옆에서 목표로 옮긴다.
+        # TODO - 아래 부분을 수정해 주세요.
+        hanoi( ?, ?, ?, ?)
+
+
+if __name__ == '__main__':
+    x = 5
+
+    hanoi( 0, 1, 2, x )
