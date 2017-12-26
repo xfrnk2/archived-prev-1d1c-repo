d1f7d5e90e81b1e5e954b2febc6f7e03074e08c6
xfrnk <xfrnk2@gmail.com>
2017-12-26 22:47:00 +0900

    Commit first programming in python

--- quiz/week1/quiz1.py
+++ quiz/week1/quiz1.py
@@ -49,17 +49,29 @@ for, list() 등의 기능을 적극 활용합니다.
 
 stone = "*"
 block = " "
-field = [ " " for x in range( 6 ) ]
-
+field = [ "*" for x in range( 6 ) ]
 
 def printField( x ):
     field[ x ] = stone
     field[ x - 1 ] = block
-    fields = ''.join( field )
+    fields = ''.join( field[1:6])
     print(fields)
 
+field1 = [ "" for x in range( 6 ) ]
+def printleft( x ):
+    field1[x] = block
+    field1[x - 1] = stone
+    fields = ''.join(field1)
+    print(fields)
 
-list = [5, 4, 3, 2, 1]
+list = [1, 2, 3, 4, 5]
+list1 = [5,4,3,2,1]
+for x in list:
+    printleft(x)
+print("    ")
+for x in list1:
+    printleft(x)
+print("    ")
 for x in list:
     printField(x)
 print("    ")
