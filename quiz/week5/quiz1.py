49fe3a1dd7189ae28ea1762121fb913d1f9f0284
xfrnk <xfrnk2@gmail.com>
2018-02-02 10:04:30 +0900

    5주차 퀴즈 -  0으로 나누는 것 때문에 ZeroDivisionError가 발생하므로 해당 코드를 주석처리,  pass를 넣어서 대체.  #20

--- quiz/week5/quiz1.py
+++ quiz/week5/quiz1.py
@@ -5,10 +5,14 @@
 from raven import Client
 
 # TODO - sentry 오류 보고 시스템에 대해서 학습합니다.
-client = Client('https://65d575d59e1748299f322af362a6b529:c4ba94596b824466a1a11631ec50623c@sentry.team504.co.kr//2')
+client = Client(
+    'https://65d575d59e1748299f322af362a6b529:c4ba94596b824466a1a11631ec50623c@sentry.team504.co.kr//2')
 
 try:
     # FIXME - 여전히 오류가 발생하고 있습니다.
-    1 / 0
+    # 1 / 0
+    # 0으로 나누는 것 때문에 ZeroDivisionError가 발생하므로 해당 코드를 주석처리,
+    # pass를 넣어서 대체.
+    pass
 except ZeroDivisionError:
     client.captureException()
