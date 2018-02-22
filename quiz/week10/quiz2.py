498f05774646df396e33d98523c2d3a8d65d3396
zrma <bulbitain@gmail.com>
2018-02-22 21:53:26 +0900

    10주차 추가 과제 업로드 #32

---
+++ quiz/week10/quiz2.py
@@ -0,0 +1,24 @@
+"""
+
+https://nipponsei.minglong.org/tracker/
+
+이 주소에 http request - get 호출을 통해 해당 페이지의 내용을 읽습니다.
+
+그 내용을 parsing 해서, 파일명 / 링크 주소 형태로 출력 해 주세요.
+
+TODO - 아래와 같이 출력 해 주세요.
+
+[Nipponsei] Marchen Madchen ED Single - sleepland [Ueda Reina].zip / http://tracker.minglong.org/torrents/%5BNipponsei%5D%20Marchen%20Madchen%20ED%20Single%20-%20sleepland%20%5BUeda%20Reina%5D.zip.torrent
+
+
+TODO - http request 는 비동기로 동작하도록 작성해 주세요.
+
+TODO - 동기 / 비동기 / 블록 / 논블록 에 대해서 학습합니다.
+
+TODO - python 의 asyncio 에 대해서 학습합니다.
+TODO - python 의 async / await 에 대해서 학습합니다.
+
+TODO - 이벤트 루프 에 대해서 학습합니다.
+TODO - python 의 asyncio.get_event_loop 에 대해서 학습합니다.
+
+"""
