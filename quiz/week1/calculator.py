d8f413082f3135606abe073e872da8c830c1e461
xfrnk <xfrnk2@gmail.com>
2018-08-11 15:19:38 +0900

    레이븐 추가

--- quiz/week1/calculator.py
+++ quiz/week1/calculator.py
@@ -213,14 +213,14 @@ def calculator():
         else:
             print("숫자를 입력하세요")
 calculator()
-# client = Client(
-#     'https://65d575d59e1748299f322af362a6b529'
-#     ':c4ba94596b824466a1a11631ec50623c@sentry.team504.co.kr//2')
-#
-# if __name__ == '__main__':
-#     # noinspection PyBroadException
-#
-#     try:
-#         calculator()
-#     except Exception:
-#         client.captureException()
+client = Client(
+    'https://65d575d59e1748299f322af362a6b529'
+    ':c4ba94596b824466a1a11631ec50623c@sentry.team504.co.kr//2')
+
+if __name__ == '__main__':
+    # noinspection PyBroadException
+
+    try:
+        calculator()
+    except Exception:
+        client.captureException()
