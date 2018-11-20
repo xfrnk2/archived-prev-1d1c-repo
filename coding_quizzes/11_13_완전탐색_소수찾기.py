af5f06a2c5afb50fa7df68ec78d28bfc4d6242fa
xfrnk <xfrnk2@gmail.com>
2018-11-21 00:50:22 +0900

    코딩퀴즈 풀기 (완성) - https://programmers.co.kr/learn/courses/30/lessons/42839

--- coding_quizzes/11_13_완전탐색_소수찾기.py
+++ coding_quizzes/11_13_완전탐색_소수찾기.py
@@ -27,56 +27,116 @@ https://kkamikoon.tistory.com/90
 """
 import random
 import itertools
-import os
+
+
 def get_numbers():
     numbers = ""
-    numbers_length = random.randrange(1, 5)
+    numbers_length = random.randrange(3, 5)
     for _ in range(numbers_length):
-        y = random.randrange(0, 4)
+        y = random.randrange(0, 10)
         y = str(y)
         numbers += y
     return numbers
 
-def main():
-    numbers = get_numbers()
-    numbers_len = len(numbers)
-    results = []
 
-    print(numbers)
-    print(numbers_len)
+def check_num(x, y):
+    while True:
+        if x < y:
+            return False
+        if y == x:
+            return True
+        if x % y == 0:
+            return False
+        else:
+            y += 1
 
-    value2 = list(numbers)
 
+def simulate(val):
+    numbers = val  # 무작위 길이의 무작위 숫자들을 숫자를 얻어온다
+    numbers_len = len(numbers)  # 숫자의 길이
 
-    for i in range(1, numbers_len+1):
+    print("얻어온 숫자의 값 : ", numbers)
+
+    value2 = list(numbers)  # 리스트화 시킨다
+
+    abc = []  # 만들수 있는 경우의 수들을 저장할 장소
+
+    for i in range(1, numbers_len + 1):  # 만들 수 있는 모든 경우의 수를 abc에 추가한다
         value = list(map(''.join, itertools.permutations(value2, i)))
-        value = list(set(value))
+        abc.extend(value)
 
-        # for y in value:
-        #     if '0' in y:
-        #         if y[0] == '0':
-        #             y = y[:1]
-        #             # value.pop(value.index(y))
-        #
-        #             for i in range(numbers_len):
-        #                 if y[i] == '0':
-        #                     y = y[:i+1]
-        #                 else:
-        #                     break
+    print("만들수 있는 모든 경우의 수", abc)
 
-        # results.append(list(map(''.join, itertools.permutations(value2, i))))
-        results.append(value)
-        for y in results[i-1]:
-            if '0' in y:
-                if y[0] == "0":
-                    print(y, "0이 있네")
-                    abc = y[1:]
-                    y = abc
-                    print(y)
-        # i개의 갯수만큼의 원소로 수열 만들기
-        # 1~ numbers_len의 길이만큼 갯수로..
+    for x in range(len(abc)):  # 문자열 좌측의 0을 모두 삭제한다
+        if '0' in abc[x]:
+            abc[x] = abc[x].lstrip("0")
 
-    print(results)
+    print(abc)
+
+    print("\n")
+    abc.sort()  # 정렬시킨다
+    abc = list(set(abc))
+    abc.sort(key=len)  # 오름차순 정렬
+    while '' in abc:  # 기존 값이 0이여서 값이''가 된  원소를 남아있지 않을 때까지 완전히 삭제시킨다
+        abc.remove('')
+
+    real = []  # 소수를 담아둘 장소
+
+    for x in range(len(abc)):  # 소수가 맞는지 확인
+        y = 2
+
+        abc[x] = int(abc[x])
+        result = check_num(abc[x], y)  # 소수 확인을 위한 함수 호출
+        if result:
+            real.append(abc[x])  # 소수이면 리스트에 저장
+        else:
+            pass
+
+    real.sort()
+    return real
+    # print("haha", abc)
+    #     for y in range(len(value)):
+    #         abc.append(value[y])
+    # abc = list(set(abc))
+    # abc.sort(key=len)
+    # print("haha",abc)
+    #
+    # for x in range(len(abc)):
+    #
+    #     if '0' in abc[x] :
+    #         abc[x].lstrip("0")
+    #     if abc[x] == '0':
+    #         abc.pop(x)
+    #
+    # print(abc)
+
+    # value = list(set(value))
+    #
+    # # for y in value:
+    # #     if '0' in y:
+    # #         if y[0] == '0':
+    # #             y = y[:1]
+    # #             # value.pop(value.index(y))
+    # #
+    # #             for i in range(numbers_len):
+    # #                 if y[i] == '0':
+    # #                     y = y[:i+1]
+    # #                 else:
+    # #                     break
+    #
+    # # results.append(list(map(''.join, itertools.permutations(value2, i))))
+    # results.append(value)
+    #
+    # for y in results[i-1]:
+    #     that = results[i - 1].index(y)
+    #     if y == '0':
+    #         print("hahaha",that)
+    #         results[i-1].pop(that)
+    #     y.lstrip("0")
+
+    # i개의 갯수만큼의 원소로 수열 만들기
+    # 1~ numbers_len의 길이만큼 갯수로..
+
     # for x in numbers:
     #     x = int(x)
     #
@@ -99,9 +159,13 @@ def main():
     #     print(i, " and ")
 
 
-                    
+def main():
+    result = simulate(get_numbers())
 
-
+    if len(result) == 0:
+        print("만들 수 있는 소수가 없습니다")
+    else:
+        print('만들수 있는 소수는..', result)
 
 
 main()
