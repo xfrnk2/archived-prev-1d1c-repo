2c362a5d052e8c7b8528b576d71ae4bfc80f0ac8
xfrnk <xfrnk2@gmail.com>
2019-03-24 15:17:24 +0900

    Repeated String (성공)

--- hackerrank/Jumping_on_the_Clouds.py
+++ hackerrank/Jumping_on_the_Clouds.py
@@ -9,31 +9,29 @@ import sys
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
-    flag = True
     steps = 0
     point = 0
 
-    while True:
-        if point == len(c) - 1:
-            break
-        else:
-            if c[point] == 0:
-                if c[point + 1] == 0:
+    while  point < len(c) - 1:
 
-                    if point == len(c) - 2:
-                        if c[point + 1] == 0:
-                            point += 1
-                    else:
-                        if c[point + 2] == 0:
-                            point += 2
-                        else:
-                            point += 1
 
+        if c[point] == 0:
+            if c[point + 1] == 0:
+
+                if point == len(c) - 2:
+                    if c[point + 1] == 0:
+                        point += 1
                 else:
-                    point += 2
-                steps += 1
+                    if c[point + 2] == 0:
+                        point += 2
+                    else:
+                        point += 1
 
-    return steps
+            else:
+                point += 2
+            steps += 1
+
+        return steps
 
 
 if __name__ == '__main__':
