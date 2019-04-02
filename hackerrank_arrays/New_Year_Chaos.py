aecf70fd43bbe304d6e1ac8d4d6e6674f5ec85b2
xfrnk <xfrnk2@gmail.com>
2019-04-02 22:05:01 +0900

    New_Year_chaos (성공) -일단은 성공했으나 코드 효율이 너무 안좋은 상태

--- hackerrank_arrays/New_Year_Chaos.py
+++ hackerrank_arrays/New_Year_Chaos.py
@@ -1,21 +1,47 @@
 import copy
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+import copy
+
+
 def minimumBribes(q):
-    e = copy.deepcopy(q)
     count = 0
+    group = {}
+    for x in range(len(q)):
+        group[q[x]] = 0
+
     for i in range(len(q), 0, -1):
         for j in range(1, i):
+
             if q[j] < q[j - 1]:
                 q[j], q[j - 1] = q[j - 1], q[j]
+                group[q[j]] += 1
                 count += 1
 
-    e.sort()
-    if count > len(q) - 1:
-        print("Too Chaotic")
-        exit()
-    if e == q:
-        print(count)
+    for y in group.values():
+        if y > 2:
+            return "Too chaotic"
 
-q = [5, 1, 2, 3, 7, 8, 6, 4]
+        else:
+            pass
+
+    return count
 
 
-print(minimumBribes(q))
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        print(minimumBribes(q))
