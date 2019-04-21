c56f91e394aac78859a23c0746392997759de32a
xfrnk <xfrnk2@gmail.com>
2019-04-21 22:14:31 +0900

    폴더명을 tdd -> tests로 변경, Travi-ci와 Coverall 뱃지 추가

--- tdd/Calculator.py
+++
@@ -1,3 +0,0 @@
-def parse(s: str) -> tuple:
-    lv, op, rv = s.split()
-    return int(lv), op, int(rv)
--- tdd/Calculator_test.py
+++
@@ -1,4 +0,0 @@
-from Calculator import parse
-class TestClass(object):
-    def test_parse(self):
-        assert parse("1 + 3") == (1, "+", 3), "공백을 잘 처리한다"
