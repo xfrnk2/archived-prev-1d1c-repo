6c97a3ea1618de773c9312e3b93ad40b0d5b7888
xfrnk <xfrnk2@gmail.com>
2019-04-21 22:14:31 +0900

    폴더명을 tdd -> tests로 변경, Travi-ci와 Coverall 뱃지 추가

--- hackerrank/Problem_Solving/Breaking_The_Records.py
+++ hackerrank/Problem_Solving/Breaking_The_Records.py
@@ -30,6 +30,7 @@ def breakingRecords(scores):
 
     return high, low
 
+<<<<<<< HEAD
 =======
  
 =======
@@ -39,6 +40,8 @@ def breakingRecords(scores):
 
 
 >>>>>>> Breaking_The_Records (성공)
+=======
+>>>>>>> 폴더명을 tdd -> tests로 변경, Travi-ci와 Coverall 뱃지 추가
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
