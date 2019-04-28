ac5a48cb839bc337fd3cfbef3d7363e72c055b09
xfrnk <xfrnk2@gmail.com>
2019-04-28 12:11:03 +0900

    no message

--- hackerrank/Problem_Solving/Breaking_The_Records.py
+++ hackerrank/Problem_Solving/Breaking_The_Records.py
@@ -25,9 +25,40 @@ def breakingRecords(scores):
             if j < lv:
                 lv = j
                 low += 1
+<<<<<<< HEAD
+<<<<<<< HEAD
 
     return high, low
 
+=======
+=======
+>>>>>>> 624d87241b727c354febfb9d7c0e15647bb6b805
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+    return high, low
+
+<<<<<<< HEAD
+=======
+ 
+=======
+
+>>>>>>> Pytest 학습, 테스트 가능한 구조로 만들기
+    return high, low
+
+
+>>>>>>> Breaking_The_Records (성공)
+=======
+>>>>>>> 폴더명을 tdd -> tests로 변경, Travi-ci와 Coverall 뱃지 추가
+<<<<<<< HEAD
+>>>>>>> 49f55fc8a24e7d557566eca47ca1dcfcb471c0dc
+=======
+=======
+
+    return high, low
+
+>>>>>>> master
+>>>>>>> 624d87241b727c354febfb9d7c0e15647bb6b805
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
