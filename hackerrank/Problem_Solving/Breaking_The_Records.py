1e7ad3a59a0a83b86319af3e305d1b081e6c9d2d
xfrnk <xfrnk2@gmail.com>
2019-05-02 21:55:46 +0900

    코드리뷰를 참고해서 코드를 수정, .coveralls.yml 파일을 생성

--- hackerrank/Problem_Solving/Breaking_The_Records.py
+++ hackerrank/Problem_Solving/Breaking_The_Records.py
@@ -10,38 +10,23 @@ import sys
 # Complete the breakingRecords function below.
 def breakingRecords(scores):
     size = len(scores)
-    high = low = hv = lv = 0
+    high = low = -1
+    hv, lv = -sys.maxsize, sys.maxsize
 
     if size == 1:
         return 0, 0
 
     for i, j in enumerate(scores):
-        if i == 0:
-            hv = lv = j
-        else:
+
             if hv < j:
                 hv = j
                 high += 1
             if j < lv:
                 lv = j
                 low += 1
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     return high, low
 
-<<<<<<< HEAD
-=======
- 
-=======
-
->>>>>>> Pytest 학습, 테스트 가능한 구조로 만들기
-    return high, low
-
-
->>>>>>> Breaking_The_Records (성공)
-=======
->>>>>>> 폴더명을 tdd -> tests로 변경, Travi-ci와 Coverall 뱃지 추가
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
