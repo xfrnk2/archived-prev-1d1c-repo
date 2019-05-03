c63c8d1aea05abc032ceba72256116cd3953b715
xfrnk <xfrnk2@gmail.com>
2019-05-03 22:35:57 +0900

    Cats_And_A_Mouse (성공)

---
+++ hackerrank/Problem_Solving/Cats_And_A_Mouse.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the catAndMouse function below.
+def catAndMouse(x, y, z):
+    if x == y:
+        return ("Mouse C")
+
+    c1 = abs(y - z)
+    c2 = abs(x - z)
+
+    if c1 > c2:
+        return ("Cat A")
+    elif c1 < c2:
+        return ("Cat B")
+    return ("Mouse C")
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
