9c3170e3b9ab9da605d56a5bda22ffcc8e6e65c4
xfrnk <xfrnk2@gmail.com>
2019-05-06 23:09:17 +0900

    Birthday_Chocolate (성공)

--- tests/Calculator_test.py
+++ tests/Calculator_test.py
@@ -2,3 +2,6 @@ from Calculator import parse
 class TestClass(object):
     def test_parse(self):
         assert parse("1 + 3") == (1, "+", 3), "공백을 잘 처리한다"
+        assert (parse("1 * 3"), (1, "*", 3))
+        assert (parse("1/ 3"), (1, "/", 3))
+        assert (parse("1-3"), (1, "-", 3), "공백이 없을 경우에도 파싱 성공한다.")
