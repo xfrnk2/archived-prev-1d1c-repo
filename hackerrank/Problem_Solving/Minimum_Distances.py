5708bd38692ec13fa88d75a1d2e7b11e374930e2
xfrnk <xfrnk2@gmail.com>
2019-06-17 22:46:05 +0900

    코드 리뷰를 참고하여 코드를 수정.(Minimum_Distances)

--- hackerrank/Problem_Solving/Minimum_Distances.py
+++ hackerrank/Problem_Solving/Minimum_Distances.py
@@ -3,19 +3,10 @@ import sys
 
 # Complete the minimumDistances function below.
 def minimumDistances(a):
-    length = len(a)
-    minDist = -1
-
-    if length == 1:
-        return minDist
-
-    for x in range(length):
-        for y in range(x + 1, length):
-            if a[x] == a[y]:
-                if minDist == -1:
-                    minDist = y - x
-                else:
-                    if y - x < minDist:
-                        minDist = y - x
-
-    return minDist
+    minDist = sys.maxsize
+    index_box = {}
+    for i, j in enumerate(a):
+        if j in index_box:
+            minDist = min(minDist, i - index_box[j])
+        index_box[j] = i
+    return minDist if minDist != sys.maxsize else -1
