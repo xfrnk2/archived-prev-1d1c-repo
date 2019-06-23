d6f872498c58fffa6216aadb26dac7b4ae925234
xfrnk <xfrnk2@gmail.com>
2019-06-24 00:14:01 +0900

    Problem_Solving.Jumping_On_The_Clouds_Revisited 함수와 테스트코드를 작성, 한가지 테스트가 실패한다.

---
+++ hackerrank/Problem_Solving/Jumping_On_The_Clouds_Revisited.py
@@ -0,0 +1,29 @@
+#https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+#Jumping_On_The_Clouds:Revisited
+
+def jumpingOnClouds(c, k):
+    n = 0
+    v = len(c) % k > 0
+    num = 100
+
+    times = len(c) // k
+
+    if not v:
+        if c[0] == 0:
+            num -= 1
+        else:
+            num -= 3
+
+    for _ in range(times):
+
+        n += k
+        if n == len(c):
+            break
+        if c[n] == 1:
+            num -= 3
+        else:
+            num -= 1
+
+    return num
+
+
