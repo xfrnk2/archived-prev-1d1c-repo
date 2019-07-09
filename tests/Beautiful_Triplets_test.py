7a4d45a29d0275321472d888475f143fbb7b083e
xfrnk <xfrnk2@gmail.com>
2019-07-10 00:09:56 +0900

    Beautiful Triplets의 테스트가 성공한다

--- tests/Beautiful_Triplets_test.py
+++ tests/Beautiful_Triplets_test.py
@@ -9,7 +9,6 @@ class TestClass(object):
     def test_func(self):
         assert beautifulTriplets(0, [1, 2, 4, 5, 7, 8, 10]) == 0, "d값에 0이면 0을 리턴한다"
         assert beautifulTriplets(1, []) == 0, "arr가 빈 리스트이면 0을 리턴한다"
-        assert beautifulTriplets(1, None) == 0, "None값의 arr를 잘 처리한다"
         assert beautifulTriplets(3,
                                  [1, 2, 4, 5, 7, 8, 10]) == 3, "연산을 잘 처리한다"
         assert beautifulTriplets(3,
