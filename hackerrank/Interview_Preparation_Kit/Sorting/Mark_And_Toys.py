dec5c13e66c27ad63ba8fd16588af73261ab7373
xfrnk <xfrnk2@gmail.com>
2019-07-15 22:52:32 +0900

    Mark_And_Toys 테스트케이스를 작성. 테스트가 실패한다

---
+++ hackerrank/Interview_Preparation_Kit/Sorting/Mark_And_Toys.py
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+def maximumToys(prices, k):
+    pass
