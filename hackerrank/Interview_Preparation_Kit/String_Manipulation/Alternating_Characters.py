09dec7a5e3ab9432ca5cd7225eb43f6b85506747
xfrnk <xfrnk2@gmail.com>
2019-07-17 22:26:10 +0900

    Alternating Characters 코드 작성

--- hackerrank/Interview_Preparation_Kit/String_Manipulation/Alternating_Characters.py
+++ hackerrank/Interview_Preparation_Kit/String_Manipulation/Alternating_Characters.py
@@ -1,2 +1,10 @@
 def alternatingCharacters(s):
-    pass
+    count = 0
+    if s == "" or type(s) is not str:
+        return count
+    front_value = None
+    for x in s:
+        if x == front_value:
+            count += 1
+        front_value = x
+    return count
