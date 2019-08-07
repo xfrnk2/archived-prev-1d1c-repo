d59c25b7a0ba324f36801eb1ac48ec27a28bb20e
xfrnk <xfrnk2@gmail.com>
2019-08-07 23:04:38 +0900

    The_Time_In_Words 테스트코드 작성, 테스트가 실패한다

--- tests/The_Time_In_Words_test.py
+++ tests/The_Time_In_Words_test.py
@@ -1,2 +1,11 @@
-def timeInWords(h, m):
-    pass
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from hackerrank.Problem_Solving.The_Time_In_Words import timeInWords
+
+
+class TestClass(object):
+    def test_func(self):
+        assert timeInWords(5, 47) == "thirteen minutes to six", "연산을 잘 수행한다"
+        assert timeInWords(3, 00) == "three o' clock", "연산을 잘 수행한다"
