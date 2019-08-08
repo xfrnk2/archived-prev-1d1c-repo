edfc374c387da98ff7abb3e464200dc48fa99e68
xfrnk <xfrnk2@gmail.com>
2019-08-09 00:30:03 +0900

    The_Time_In_Words 코드를 리팩토링, 문제없이 돌아간다.

--- hackerrank/Problem_Solving/The_Time_In_Words.py
+++ hackerrank/Problem_Solving/The_Time_In_Words.py
@@ -1,37 +1,35 @@
 def timeInWords(h, m):
-    words = {"00": "o' clock", "01": "one", "02": "two", "03": "three",
-             "04": "four", "05": "five", "06": "six", "07": "seven",
-             "08": "eight",
-             "09": "nine", "10": "ten", "11": "eleven", "12": "twelve",
-             "13": "thirteen",
-             "14": "fourteen", "15": "quarter", "16": "sixteen",
-             "17": "seventeen", "18": "eighteen",
-             "19": "nineteen", "20": "twelve", "21": "twenty one",
-             "22": "twenty two", "23": "twenty three",
-             "24": "twenty four", "25": "twenty five", "26": "twenty six",
-             "27": "twenty seven",
-             "28": "twenty eight", "29": "twenty nine", "30": "half"}
-    h, m = str(h), str(m)
+    words = {00: "o' clock", 1: "one", 2: "two", 3: "three",
+             4: "four", 5: "five", 6: "six", 7: "seven",
+             8: "eight",
+             9: "nine", 10: "ten", 11: "eleven", 12: "twelve",
+             13: "thirteen",
+             14: "fourteen", 15: "quarter", 16: "sixteen",
+             17: "seventeen", 18: "eighteen",
+             19: "nineteen", 20: "twelve", 21: "twenty one",
+             22: "twenty two", 23: "twenty three",
+             24: "twenty four", 25: "twenty five", 26: "twenty six",
+             27: "twenty seven",
+             28: "twenty eight", 29: "twenty nine", 30: "half"}
 
-    if m == "0":
-        return words["0" + h] + " " + words[m + m]
+    minutes, flag, hours = "", " past ", " "
 
-    flag = "past"
-    if 30 < int(m):
-        flag = "to"
-        h = int(h) + 1
-        m = 60 - int(m)
-        h, m = str(h), str(m)
+    if m == 0:
+        return words[h] + " " + words[m]
 
-    if m == "30" or m == "15":
+    if 30 < m:
+        flag = " to "
+        m, h = 60 - m, h + 1
 
-        if 10 <= int(h):
-            return words[m] + " " + flag + " " + words[h]
-        return words[m] + " " + flag + " " + words["0" + h]
+    minutes = words[m]
+    hours = words[h]
+
+    if m % 15 == 0:
+        pass
     else:
-        if 10 <= int(h):
-            return words[m] + " " + "minutes" + " " + flag + " " + words[h]
+        v = " minute"
+        if 1 < m <= 60:
+            v += "s"
+        minutes += v
 
-        return words[m] + " " + "minutes" + " " + flag + " " + words["0" + h]
-
-
+    return minutes + flag + hours
