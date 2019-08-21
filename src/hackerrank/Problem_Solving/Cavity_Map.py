22101c4335154a388f26349899bc140da86c87dd
xfrnk <xfrnk2@gmail.com>
2019-08-21 23:53:13 +0900

    Cavity_Map 코드 작성. 테스트 실패.

--- src/hackerrank/Problem_Solving/Cavity_Map.py
+++ src/hackerrank/Problem_Solving/Cavity_Map.py
@@ -1,18 +1,18 @@
 def cavityMap(grid):
-    g_length = len(grid)
-    j_length = 0
-    value = 0
-
-    if g_length <= 2:
+    if len(grid) <= 2 or len(grid[0]) <= 2:
         return grid
 
-    for i, j in enumerate(grid):
-        j_length = len(j)
-        if i == 0:
-            if j_length <= 2:
-                return grid
+    box = []
+    x_value = grid[1:-1]
+    target = 0
 
-        for x in j[1:-1]:
-            if value < x:
-                value = x
+    for x in x_value:
+        box.extend(x[1:-1])
+    target = max(box)
+
+    for x in range(1, len(grid) - 1):
+        for y in range(1, len(grid[x]) - 1):
+
+            if grid[x][y] == target:
+                pass
     return grid
