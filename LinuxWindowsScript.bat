ab7f0af1ff4fa8a7ff91dad34d0ef1a778539479
xfrnk <xfrnk2@gmail.com>
2019-08-28 00:15:21 +0900

    LinuxWindowsScript.bat 파일에 대한 테스트

--- LinuxWindowsScript.bat
+++ LinuxWindowsScript.bat
@@ -1,3 +1,20 @@
+:<<"::CMDLITERAL"
+@ECHO OFF
+GOTO :CMDSCRIPT
+::CMDLITERAL
+
+echo "I can write free-form ${SHELL} now!"
+if :; then
+  echo "This makes conditional constructs so much easier because"
+  echo "they can now span multiple lines."
+fi
+exit $?
+
+:CMDSCRIPT
+ECHO Welcome to %COMSPEC%
+
+
+
 echo >/dev/null # >nul & GOTO WINDOWS & rem ^
 echo 'Processing for Linux'
 
