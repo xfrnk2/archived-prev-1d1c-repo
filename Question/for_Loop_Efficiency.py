716c884c2b92972dc914b66033798d3829e7fc37
xfrnk2 <xfrnk2@gmail.com>
2020-01-20 00:46:19 +0900

    #241 코드리뷰 반영

--- Question/for_Loop_Efficiency.py
+++ Question/for_Loop_Efficiency.py
@@ -1,34 +1,40 @@
 import time
 from itertools import product
-
-def simulation1(SUITS, RANKS):
+from typing import Tuple, Sequence
+def simulation1(lhs: Sequence[str], rhs: Sequence[str]) -> Tuple[list, float]:
     #progressing start
     start_time = time.time()
 
 
-    deck = [(s, r) for r in RANKS for s in SUITS]
+    deck = [(s, r) for r in rhs for s in lhs]
 
 
     #progressing end
     end_time = time.time()
-    return end_time - start_time
+    return deck, end_time - start_time
 
-def simulation2(SUITS, RANKS):
+def simulation2(lhs: Sequence[str], rhs: Sequence[str]) -> Tuple[list, float]:
     # progressing start
     start_time = time.time()
 
-    deck = list(product(RANKS, SUITS))
+    deck = list(product(lhs, rhs))
 
     # progressing end
     end_time = time.time()
-    return end_time - start_time
+    return deck, end_time - start_time
 
 if __name__ == "__main__":
 
-    SUITS = "♠ ♡ ♢ ♣".split() * 1000
-    RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split() * 1000
-    print(simulation1(SUITS, RANKS))
-    print(simulation1(SUITS, RANKS))
+    suits = "♠ ♡ ♢ ♣".split() * 1000
+    ranks = "2 3 4 5 6 7 8 9 10 J Q K A".split() * 1000
+
+    for _ in range(5):
+        deck1, time1 = simulation1(suits, ranks)
+        deck2, time2 = simulation1(suits, ranks)
+
+        assert deck1 == deck2
+        print(time1, time2, "\n")
+
 
     #result
     #simulation1 = 6.825436353683472
