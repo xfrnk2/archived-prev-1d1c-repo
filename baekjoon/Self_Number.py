2965846efe5f4e43e7970f4f66116c9c06116717
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-02-13 00:06:40 +0900

    바르게 풀이가 되지 않아 처음부터 다시 풀이함

--- baekjoon/Self_Number.py
+++ baekjoon/Self_Number.py
@@ -1,25 +1,16 @@
 #https://www.acmicpc.net/problem/4673
 
-arr = []
-l = []
+arr = set()
+group = [i for i in range(1, 10000)]
+for x in group:
+    if 10000 < x:
+        break
+    temp = x
+    x = list(str(x))
+    #셀프넘버
+    temp += eval("+".join(x))
+    arr.add(temp)
 
-for x in range(1, 10000):
-    if x // 10 < 1:
-        # 한자리
-        arr.append(x + x)
-    if x // 10 < 10:
-        # 두자리
-        arr.append(x + x // 10 + x % 10)
-    if x // 100 < 10:
-        # 세자리
-        arr.append(x + x // 100 + x // 10 + x % 10)
-    if x // 1000 < 10:
-        # 네자리
-        arr.append(x + x // 1000 + x // 100 + x // 10 + x % 10)
-    if x == 10000:
-        arr.append(x + x // 10000 + x // 1000 + x // 100 + x // 10 + x % 10)
-    if not x in arr:
-        l.append(x)
-
-
-print(l)
+for x in group:
+    if x not in arr:
+        print(x)
