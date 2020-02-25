9586091b46418f473057df2bd4b5904c71276ac6
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-02-25 18:19:08 +0900

    백준 11720 숫자의합 풀이

---
+++ baekjoon/Number_Sum.py
@@ -0,0 +1,14 @@
+#https://www.acmicpc.net/problem/11720
+def func(n, num):
+    if n <= 0:
+        return 0
+    return int(str(num)[n-1]) + func(n - 1, num)
+
+def func2(num):
+    return eval('+'.join(num))
+
+
+if __name__ == '__main__':
+   value = [int(input()) for _ in range(2)]
+   print(func(value[0], value[1]))
+   print(func2(str(value[1])))
