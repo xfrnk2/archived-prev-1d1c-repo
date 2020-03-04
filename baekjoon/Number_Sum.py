1fcd731e562750672e11cb978830a8d12731f0ea
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-03-05 01:41:09 +0900

    재귀함수를 사용한 함수를 제거

--- baekjoon/Number_Sum.py
+++ baekjoon/Number_Sum.py
@@ -1,13 +1,7 @@
 #https://www.acmicpc.net/problem/11720
-def func(n, num):
-    if n <= 0:
-        return 0
-    return int(str(num)[n-1]) + func(n - 1, num)
-
-def func2(num):
+def function(num):
     return eval('+'.join(num))
 
 if __name__ == '__main__':
    value = [int(input()) for _ in range(2)]
-   print(func(value[0], value[1]))
-   print(func2(str(value[1])))
+   print(function(str(value[1])))
