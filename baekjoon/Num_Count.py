e11df94dbadef7c25bf87b027875d3cab36902c7
DESKTOP-K28GGME\rad87_home <xfrnk2@gmail.com>
2020-03-11 17:55:00 +0900

    코드리뷰-#256 를 참고해서 코드를 수정하고 꼬리재귀함수와 재귀함수의 차이점이 무엇인지 알아보았다.

--- baekjoon/Num_Count.py
+++ baekjoon/Num_Count.py
@@ -5,6 +5,10 @@
 You can see in the documentation what the difference is. reduce returns a single result,
  the sum, product, etc., of the sequence. accumulate returns an iterator over all the intermediate results.
   Basically, accumulate returns an iterator over the results of each step of the reduce operation.
+
+아래는 표현예시
+sum(accumulate(arr, add))
+reduce(arr)
 """
 #eval('*'.join([input() for _ in range(3)]))
 #reduce(lambda x, y : x*y, arr)
