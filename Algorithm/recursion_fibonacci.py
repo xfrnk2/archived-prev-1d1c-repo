e259aac4bbc1981b9483708554ff1f3ea8f23d55
DESKTOP-K28GGME\rad87_home <xfrnk2@gmail.com>
2020-03-13 22:39:44 +0900

    피보나치 꼬리재귀, 재귀형태 최대공약수 구하기 복습

--- Algorithm/recursion_fibonacci.py
+++ Algorithm/recursion_fibonacci.py
@@ -5,3 +5,14 @@ def fibonacci(n: int) -> int:
     return fibonacci(n-1) + fibonacci(n-2)
 
 print(fibonacci(10))
+
+
+
+
+def fibo_tail(n : int, lhs: int, rhs: int):
+
+    if n == 1:
+        return rhs
+    return fibo_tail(n-1, rhs, lhs + rhs)
+
+print(fibo_tail(5, 0, 1))
