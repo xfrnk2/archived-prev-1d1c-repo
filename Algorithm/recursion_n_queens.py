bb73f41ec022060bb4e1b42a8222641d0f409f42
DESKTOP-K28GGME\rad87_home <xfrnk2@gmail.com>
2020-03-16 17:38:09 +0900

    합병 정렬 구현

--- Algorithm/recursion_n_queens.py
+++ Algorithm/recursion_n_queens.py
@@ -1,26 +1,48 @@
+#
+# n = 8
+# cols = [0 for _ in range(n)]
+# def queens(level):
+#     print(cols)
+#     if not promising(level):
+#         return False
+#     elif level==n:
+#         return True
+#     else:
+#         for i in range(1, n+1):
+#             cols[level+1] = i
+#             if queens(level+1):
+#                 return True
+#         return False
+#
+# def promising(level):
+#     for x in range(1, level):
+#         if cols[x] == cols[level]: #x는 말의 번호이다. level번째 말과 열이 겹치는지 확인한다.
+#             return False
+#         elif level-x == abs(cols[level]-cols[x]): # level-x는 곧 행의 차이다.
+#             return False
+#     return True
+#
+# queens(n)
+# print(cols)
 
-n = 8
-cols = [0 for _ in range(n)]
-def queens(level):
-    print(cols)
-    if not promising(level):
-        return False
-    elif level==n:
-        return True
-    else:
-        for i in range(1, n+1):
-            cols[level+1] = i
-            if queens(level+1):
-                return True
-        return False
 
-def promising(level):
-    for x in range(1, level):
-        if cols[x] == cols[level]: #x는 말의 번호이다. level번째 말과 열이 겹치는지 확인한다.
-            return False
-        elif level-x == abs(cols[level]-cols[x]): # level-x는 곧 행의 차이다.
-            return False
-    return True
 
-queens(n)
-print(cols)
+size = 5
+c = [0 for x in range(size+1)]
+
+def queen_func(value: int):
+    if prom(value):
+
+def prom(value: int):
+    for i in range(1, size+1):
+        if 
+
+
+
+
+
+
+
+queen_func(0)
+
+
---
+++ Algorithm/sort_merge_sort.py
@@ -0,0 +1,32 @@
+def merge_sort(arr):
+    if 1 < len(arr):
+        mid = len(arr)//2
+        l = arr[:mid]
+        r = arr[mid:]
+
+        left = merge_sort(l)
+        right = merge_sort(r)
+        return merge(left, right)
+    return arr
+
+def merge(left, right):
+    i, j = 0, 0
+    temp = []
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            temp.append(left[i])
+            i +=1
+        else:
+            temp.append(right[j])
+            j += 1
+    while i < len(left):
+        temp.append(left[i])
+        i+=1
+    while i < len(left):
+        temp.append(right[j])
+        j+=1
+    return temp
+
+arr = [5, 3, 4, 2, 1]
+print(merge_sort(arr))
