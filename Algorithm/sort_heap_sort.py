55ce3129f2a18742a230a769e5f19c978d6c2425
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-03-25 13:35:27 +0900

    함수명을 max_heapify로 변경

--- Algorithm/sort_heap_sort.py
+++ Algorithm/sort_heap_sort.py
@@ -2,7 +2,7 @@
 # 조건 2. 트리 모양은 complete-binary-tree 여야 하고, 왼쪽 서브트리와 오른쪽 서브트리가 그들 자체로 'heap'인 상태이다.
 # 위와 같은 조건에서 'max-heap'을 만들어 주는 함수를 아래의 heapify(arr,i)로 구현한다.
 
-def heapify(arr, current_index, size):
+def max_heapify(arr, current_index, size):
     left_child_index, right_child_index = (current_index+1)*2-1, (current_index+1)*2
     target_index = left_child_index
 
@@ -23,7 +23,7 @@ def heapify(arr, current_index, size):
     #'target_index'에 위치한 값과 'current_index'에 위치한 값을 비교 연산을 통해 스왑한다.
     if arr[current_index] < arr[target_index]:
         arr[target_index], arr[current_index] = arr[current_index], arr[target_index]
-        return heapify(arr, target_index, size)
+        return max_heapify(arr, target_index, size)
 
     #자식 노드가 전부 루트 노드보다 작은 값일 경우 종료한다.
     else:
@@ -32,7 +32,7 @@ def heapify(arr, current_index, size):
 
 def heap_sort(arr, size):
     #배열 arr 를 'max-heap'화 한다.
-    heapify(arr, 0, len(arr))
+    max_heapify(arr, 0, len(arr))
     #'max-heap'을 출력한다.
     print(arr)
 
@@ -40,7 +40,7 @@ def heap_sort(arr, size):
     for max_index in range(size-1, 0, -1):
         arr[max_index], arr[0] = arr[0], arr[max_index]
         #제일 마지막 위치에 최댓값이 저장되면 더이상 비교연산이 필요없어지므로 전체 범위(size)를 'max_index'로 한다.
-        heapify(arr, 0, max_index)
+        max_heapify(arr, 0, max_index)
 
 
 arr = [4, 16, 15, 8, 7, 13, 14, 2, 5]
