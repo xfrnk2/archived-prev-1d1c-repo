4d55119e2a331de13a8eb03e9b711ada0ead3b9c
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-04-18 17:18:52 +0900

    test

--- LinuxWindowsScript.sh
+++ LinuxWindowsScript.sh
@@ -11,7 +11,7 @@ echo 'Processing for Linux'
 
 # Do Linux Bash commands here... for example:
 StartDir="$(pwd)"
-$ source venv/bin/activate
+$ source . venv/bin/activate
 # Then, when all Linux commands are complete, end the script with 'exit'...
 exit 0
 
@@ -22,5 +22,5 @@ echo "Processing for Windows"
 
 REM Do Windows CMD commands here... for example:
 SET StartDir=%cd%
-$ call venv/Scripts/activate.bat
+$ call . venv/Scripts/activate.bat 
 REM Then, when all Windows commands are complete... the script is done.
