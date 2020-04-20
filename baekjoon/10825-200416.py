bc85683a4bf0f38991eaa48c09631811f9e59199
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-04-21 02:15:55 +0900

    BOJ-10825 코드 주석 추가

--- baekjoon/10825-200416.py
+++ baekjoon/10825-200416.py
@@ -2,11 +2,12 @@
 #주소 : https://www.acmicpc.net/problem/10825
 #실마리 얻은곳 : https://dailyheumsi.tistory.com/67
 # https://wayhome25.github.io/python/2017/03/07/key-function/
-from operator import itemgetter
+
 if __name__ == '__main__':
 
     student_list = [list(map(lambda x: int(x) if x.isdecimal() else x, input().split())) for _ in range(int(input()))]
     student_list.sort(key=lambda student: (-student[1], student[2], -student[3], student[0]))
-    # key=itemgetter(-1, 2, -3, 0)도 가능하다.
     for x in student_list: print(x[0])
+    # key=itemgetter(-1, 2, -3, 0)은 불가능하다. reverse 속성을 하나하나 부여할수 없기 때문. -는 뒤에서부터의 인덱스로 적용된다.
+
 
