7e84d5ca85ddd13d26bc222cb95310cf7a3ff1a5
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-04-21 02:04:29 +0900

    BOJ-10825 코드 더 짧게 리팩토링

--- baekjoon/10825-200416.py
+++ baekjoon/10825-200416.py
@@ -2,18 +2,11 @@
 #주소 : https://www.acmicpc.net/problem/10825
 #실마리 얻은곳 : https://dailyheumsi.tistory.com/67
 # https://wayhome25.github.io/python/2017/03/07/key-function/
-
+from operator import itemgetter
 if __name__ == '__main__':
-    student_count = int(input())
-    students_list = []
 
-    for _ in range(student_count):
-        student = input().split()
-        student = student[:1] + list(map(int, student[1:]))
-        students_list.append(student)
-
-    students_list = sorted(students_list, key = lambda student : (-student[1], student[2], -student[3], student[0]))
-
-    for x in students_list:
-        print(x[0])
+    student_list = [list(map(lambda x: int(x) if x.isdecimal() else x, input().split())) for _ in range(int(input()))]
+    student_list.sort(key=lambda student: (-student[1], student[2], -student[3], student[0]))
+    # key=itemgetter(-1, 2, -3, 0)도 가능하다.
+    for x in student_list: print(x[0])
 
