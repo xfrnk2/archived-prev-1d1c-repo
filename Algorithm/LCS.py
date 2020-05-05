122a38526bb7835eee5c4186aa7046f466e4b2e7
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-05-05 12:33:50 +0900

    LCS를 표준적 방법으루 구현

--- Algorithm/LCS.py
+++ Algorithm/LCS.py
@@ -11,11 +11,27 @@ def lcs(a, b):
         prev = current
     return prev[-1]
 
-S1 = "THESTRINGS"
+S1 = "THESTRINGS" # ans = 5
 S2 = "THISISIT"
-# S1 = "ABCDA"
+# S1 = "ABCDA" # ans = 4
 # S2 = "ACBDEA"
 print(lcs(S1, S2))
+
+def lcs2(a, b):
+    arr = [[] for _ in range(len(a)) for _ in range(len(b))]
+
+    for x in range(len(a)):
+        for y in range(len(b)):
+            if a[x] == 0 or b[y] == 0:
+                arr[x][y] = 0
+            elif a[x] == b[y]:
+                arr[x][y] = arr[x-1][y-1]
+            elif a[x] != b[y]:
+                arr[x][y] = max(a[x-1][y], a[x][y-1])
+    return arr[-1][-1]
+
+print(lcs(S1, S2))
+
 
 
 
