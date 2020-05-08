5e1fc67c8a4c87614e937976f3773a8687e91966
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-05-08 10:14:14 +0900

    2byte뿐만 아니라 모든 조건의 행과 열의 길이에서 동작하도록 k, i, j의 range를 수정

--- Algorithm/matrix_multiply_2byte.py
+++ Algorithm/matrix_multiply_2byte.py
@@ -1,13 +1,18 @@
+# 이해를 도와준 사이트 : https://rednooby.tistory.com/81
+
 def matrixMul2Byte(a, b):
 
-    assert len(a[0]) == len(b), "앞 행렬의 열 갯수, 뒤 행렬의 행 갯수가 불일치"
+    ar, ac = len(a), len(a[0])
+    br, bc = len(b), len(b[0])
+
+    assert ac == br, "앞 행렬의 열 갯수, 뒤 행렬의 행 갯수가 불일치"
 
     matrix = []
-    for k in range(2):
+    for k in range(ac):
         answer = []
-        for i in range(2):
+        for i in range(ar):
             result = 0
-            for j in range(2):
+            for j in range(ac):
                 mul = a[k][j] * b[j][i]
                 print(f"k : {k}, i : {i}, j : {j}")
                 print(f"a[k][j] : {a[k][j]}, b[j][i] : {b[j][i]}, mul : {mul}")
