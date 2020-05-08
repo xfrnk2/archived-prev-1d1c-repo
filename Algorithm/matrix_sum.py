e4be675ea8a1350a4fb06be4467784340d7a210d
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-05-08 10:01:07 +0900

    행렬의 덧셈 구현

--- Algorithm/matrix_sum.py
+++ Algorithm/matrix_sum.py
@@ -20,4 +20,4 @@ def sumMatrix(a, b):
 
 print(sumMatrix([[1,2], [2,3]], [[3,4],[5,6]]))
 print()
-print(sumMatrix([[1,3,5], [2,4,6]], [[2,4,7],[1,5,3]]))
+print(sumMatrix([[1,3,5], [2,4,6]], [[2,4,7],[1,5,3]])) 
