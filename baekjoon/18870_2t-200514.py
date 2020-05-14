0e18fd4af0faf51ebb696deb73cdcb2cda9970e5
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-05-14 19:36:33 +0900

    BOJ-1991-트리 순회 2차 풀이, 새롭게 코드 작성

--- baekjoon/18870_2t-200514.py
+++ baekjoon/18870_2t-200514.py
@@ -1,8 +1,9 @@
+# 링크 : https://www.acmicpc.net/problem/18870
+
 input()
 arr = list(map(int, input().split()))
-another_arr = sorted(set(arr))
-value_indices = {}
-for i,j in enumerate(another_arr):
-    value_indices[j] = i
+values_by_key = {}
+for i,j in enumerate(sorted(set(arr))):
+    values_by_key[j] = i
 for x in arr:
-    print(value_indices[x], end=' ')
+    print(values_by_key[x], end=' ')
