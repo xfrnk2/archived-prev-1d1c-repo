5c44e82ece0c56b9751db301d87b902d47561d8a
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-06-02 15:11:13 +0900

    뿌요뿌요 블럭 낙하 구현

--- myproject/puyopuyo.py
+++ myproject/puyopuyo.py
@@ -1,21 +1,51 @@
-import time
 import os
+import threading
+import keyboard
+from copy import deepcopy
+field_width, field_height = 6, 12
+field = [['□' for _ in range(field_width)] for _ in range(field_height)]
+cursorX, cursorY = 2, -2
+checked = deepcopy(field)
+
+class AsyncTask:
+
+    def __init__(self):
+        pass
+    def TaskA(self):
+        #로직 바꾸기
+        global cursorY
+        cursorY += 1
+        threading.Timer(1,self.TaskA).start()
+
+    def TaskB(self):
+        print('Precess B')
+        threading.Timer(3, self.TaskB).start()
+
 
 def func():
-    arr = ['□' * 6 for _ in range(12)]
-    c = 0
+    global checked, cursorX, cursorY
+
+    at = AsyncTask()
+    at.TaskA()
+    at.TaskB()
+
     while True:
         os.system('cls')
-
-        for i in arr:
-            print(i)
-
-        time.sleep(0.3)
-        c += 1
-        print(c)
-
-
-
+        for y in range(field_height):
+            for x in range(field_width):
+                if x == cursorX and y == cursorY:
+                    print("■", end='')
+                else:
+                    print(field[y][x], end='')
+            print('')
+        if keyboard.is_pressed('d'):
+            cursorX += 1
+        elif keyboard.is_pressed('a'):
+            cursorX -= 1
+        elif keyboard.is_pressed('w'):
+            cursorY -= 1
+        elif keyboard.is_pressed('s'):
+            cursorY += 1
 
 if __name__ == '__main__':
     func()
