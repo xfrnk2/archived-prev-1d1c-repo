e7dce5c1e11ac28794706c9cf8af71aae27cd41a
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-06-02 15:54:54 +0900

    뿌요뿌요 종료조건 구현, 낙하 후 블록의 위치를 필드에 적용, 필드 밖으로 벗어나지 못하도록 제한을 둠

--- myproject/puyopuyo.py
+++ myproject/puyopuyo.py
@@ -2,17 +2,26 @@ import os
 import threading
 import keyboard
 from copy import deepcopy
+
+
+
+class Block:
+    def __init__(self):
+        self.__block = "□"
+    def __str__(self):
+        return self.__block
+
 field_width, field_height = 6, 12
-field = [['□' for _ in range(field_width)] for _ in range(field_height)]
-cursorX, cursorY = 2, -2
+field = [[Block() for _ in range(field_width)] for _ in range(field_height)]
+cursorX, cursorY = 2, 0
 checked = deepcopy(field)
+another_block = "■"
 
 class AsyncTask:
 
     def __init__(self):
         pass
     def TaskA(self):
-        #로직 바꾸기
         global cursorY
         cursorY += 1
         threading.Timer(1,self.TaskA).start()
@@ -27,7 +36,6 @@ def func():
 
     at = AsyncTask()
     at.TaskA()
-    at.TaskB()
 
     while True:
         os.system('cls')
@@ -38,12 +46,21 @@ def func():
                 else:
                     print(field[y][x], end='')
             print('')
+
+        if set(field[x][2] for x in range(field_height)) == set(another_block):
+            print("game over")
+
+        if cursorY == 11 or field[cursorY+1][cursorX] == another_block:
+            field[cursorY][cursorX] = another_block
+            cursorX, cursorY = 2, -2
+
+
         if keyboard.is_pressed('d'):
-            cursorX += 1
+            if cursorX != field_width-1 and field[cursorY][cursorX+1] != another_block:
+                cursorX += 1
         elif keyboard.is_pressed('a'):
-            cursorX -= 1
-        elif keyboard.is_pressed('w'):
-            cursorY -= 1
+            if cursorX != 0 and field[cursorY][cursorX-1] != another_block:
+                cursorX -= 1
         elif keyboard.is_pressed('s'):
             cursorY += 1
 
