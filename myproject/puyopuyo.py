5bbe6d484e8228d4711196efbf8294ce2ab86af0
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-06-29 00:23:07 +0900

    연쇄 기능을 구현

--- myproject/puyopuyo.py
+++ myproject/puyopuyo.py
@@ -78,12 +78,12 @@ def checkField(x: int, y: int, shape:str):
                     stack = list(set(stack))
                     stack.extend([(a - 1, b), (a + 1, b), (a, b + 1), (a, b - 1)])
 
-    print(target)
+    # print(target)
     return list(set(target))
 
 
 
-def ignition(target_list):
+def ignition(target_list, at):
     for target in target_list:
         if 4 <= len(target):
             row_info = {v:[0,0] for v in range(field_width)}#행 x에 해당하는 (y의 최저점, 블록의 양)
@@ -103,13 +103,17 @@ def ignition(target_list):
                     for i in range(lowest, 0, -1):
                         field[i][x], field[i-1][x] = field[i-1][x], field[i][x]
 
+            # at.stopTaskA()
+            # sleep(0.3)
+            # at.TaskA()
+
             rensa_target_list = []
             for i in range(field_width):
                 if 0 < row_info[i][1]:
                     for j in range(row_info[i][0], -1, -1):
                         if field[j][i].get_shape() != "□":
                            rensa_target_list += [eval(f"checkField({i}, {j}, field[{j}][{i}].get_shape())")]
-            ignition(rensa_target_list)
+            ignition(rensa_target_list, at)
 
 
 
@@ -148,9 +152,13 @@ def func():
 
         if cursorY == 11 or subCursorY == 11 or field[subCursorY+1][subCursorX].get_shape() in blocks or field[cursorY+1][cursorX].get_shape() in blocks:
 
-            if cursorY == 11 or subCursorY == 11:
+            if cursorY == 11:
                 field[cursorY][cursorX].set_block(current_block[0])
                 field[subCursorY][subCursorX].set_block(current_block[1])
+
+            elif subCursorY == 11:
+                field[subCursorY][subCursorX].set_block(current_block[1])
+                field[cursorY][cursorX].set_block(current_block[0])
 
             elif abs(subCursorY-cursorY) == 1:
                 field[cursorY][cursorX].set_block(current_block[0])
@@ -192,7 +200,7 @@ def func():
             for pair in cursor_list:
                 x, y = pair
                 target_list += [eval(f"checkField({x}, {y}, field[{y}][{x}].get_shape())")]
-            ignition(target_list)
+            ignition(target_list, at)
 
                 # shape_list = []
                 # blocked_list = []
