3e35391a743d07da17bae01611cb0b1a776f6150
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-07-01 17:47:45 +0900

    셸 정렬을 구현

--- Algorithm/sort_shell_sort.py
+++ Algorithm/sort_shell_sort.py
@@ -1,19 +1,21 @@
 def insertionSort(arr, start, interval):
-    for i in range(start, len(arr), interval):
+    for i in range(start+interval, len(arr), interval):
         current_value = arr[i]
-        while interval <= i and arr[i] < arr[i-interval]:
-            arr[i] = arr[i-interval]
-            arr[i-interval] = current_value
+        position = i
+        while interval <= position and arr[position] < arr[position-interval]:
+            arr[position] = arr[position-interval]
+            position = position - interval
+        arr[position] = current_value
 
 def shellSort(arr):
     interval = len(arr)//2
-    while 1 <= interval:
+    while 0 < interval:
         for x in range(interval):
             insertionSort(arr, x, interval)
             print(arr)
         interval = interval//2
 
-
+shellSort([1, 5, 20, 4, 7, 2, 8, 9])
 
 
 
@@ -21,4 +23,4 @@ def shellSort(arr):
 
 
 arr = [0, 8, 11, 6, 2, 13, 7, 30]
-print(shellSort(arr))
+shellSort(arr)
