a1d1059f43edf01b3ef233f6507de65bb098b594
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-07-19 12:37:07 +0900

    프로그래머스 lv2 멀쩡한 사각형 풀이

--- programmers/common_square.py
+++ programmers/common_square.py
@@ -2,17 +2,9 @@
 
 
 def solution(w, h):
-    x = 0
-    f = 2*w - h
-    df1 = 2* w
-    df2 = 2*(w-h)
-    stack = []
-    for y in range(h):
-        stack.append((x, y))
-        if f < 0:
-            f += df1
-        else:
-            x += 1
-            f += df2
-    return stack
-print(len(solution(8, 12)))
+    m, n = w, h
+    while 0 < n:
+        m, n = n, m%n
+    squares = w+h-1 if m==1 else w+h-m
+    return w*h - squares
+print(solution(8, 12))
