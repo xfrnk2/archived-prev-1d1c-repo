2d68293afecd42842e23cc9065ea5e1c3501f597
xfrnk2 <xfrnk2@gmail.com>
2020-08-28 18:35:35 +0900

    근묵자흑 문제 간결화...

--- myproject/brandy_2020_coding_test_codenameB_quiz.py
+++ myproject/brandy_2020_coding_test_codenameB_quiz.py
@@ -16,3 +16,11 @@ print(func([7, 3, 1, 8, 4, 6, 2, 5], 8, 3))
 example3 = [5]*37
 example3[4] = 1
 print(func(example3, 37, 4))
+
+def func_second(arr, N, k):
+    print((N - arr.index(min(arr))) // (k-1)+1)
+
+
+func_second([2, 3, 1, 4], 4, 3)
+func_second([7, 3, 1, 8, 4, 6, 2, 5], 8, 3)
+func_second(example3, 37, 4)
