85fcb94186a5dcad796239c7c0d6ff30435662b7
xfrnk2 <xfrnk2@gmail.com>
2020-09-10 02:04:36 +0900

    백준 2079 팰린드롬 생각난대로 짜본것. 문제상의 예시는 성공하지만 백준 사이트에서 실행하면 런타임 에러가 뜬다.. 고민을 더 해봐야 알 것 같다.

---
+++ baekjoon/2079-200909.py
@@ -0,0 +1,64 @@
+#https://www.acmicpc.net/submit/2079
+arr = 'anavolimilana'
+arr2 =  'abaccbcb'
+
+def search_area(arr, start, k, dup, c, f):
+
+    if f:
+        if len(arr) <= k:
+            return dup
+
+        if arr[k-c] == arr[k]:
+            dup += arr[k]
+            c += 2
+            return search_area(arr, start, k + 1, dup, c, f)
+        else:
+            # if k < c:
+            if k-c+1 == start:
+                return dup
+            else:
+                target = dup[0]
+                box = ''
+                for x in dup:
+                    if x == target:
+                        box += x
+                    else:
+                        break
+                if 1 < len(box):
+                    return box
+                else:
+                    return dup[0]
+
+
+
+    else:
+        if not dup:
+            return search_area(arr, start, k+1, arr[start], c, f)
+
+        if arr[k] == dup[-1]:
+
+            return search_area(arr, start, k+1, dup + dup[-1], c, f)
+
+        if arr[k] not in dup:
+            dup += arr[k]
+            return search_area(arr, start, k+1, dup, c, f)
+
+        elif arr[k-c] == arr[k]:
+            dup += arr[k]
+            c += 2
+            f = not f
+            return search_area(arr, start, k+1, dup, c, f)
+
+    return dup
+results = []
+
+i = 0
+while i < len(arr):
+    value = search_area(arr, i, i, '', 2, False)
+    if value == arr[i]:
+        i += 1
+    else:
+        i += len(value)
+    results.append(value)
+print(len(results))
+
