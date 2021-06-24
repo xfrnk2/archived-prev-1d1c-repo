6b63f9387d8f71c974df641f9b70a359f5a8453b
xfrnk2 <xfrnk2@gmail.com>
2021-06-24 16:16:47 +0900

    파일 이름 변경

---
+++ codeit_camp/algorithms/1-2_linear_search.py
@@ -0,0 +1,12 @@
+def linear_search(element, some_list):
+    for i, j in enumerate(some_list):
+        if element == j:
+            return i
+    return None
+
+
+print(linear_search(2, [2, 3, 5, 7, 11]))
+print(linear_search(0, [2, 3, 5, 7, 11]))
+print(linear_search(5, [2, 3, 5, 7, 11]))
+print(linear_search(3, [2, 3, 5, 7, 11]))
+print(linear_search(11, [2, 3, 5, 7, 11]))
