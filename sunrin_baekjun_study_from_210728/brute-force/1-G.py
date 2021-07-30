c3a963abba63f646bd2d7b5227d59baed74b74d4
xfrnk2 <xfrnk2@gmail.com>
2021-07-31 01:40:52 +0900

    선린 알고리즘 스터디 완전탐색 G

--- sunrin_baekjun_study_from_210728/brute-force/1-G.py
+++ sunrin_baekjun_study_from_210728/brute-force/1-G.py
@@ -1,19 +1,37 @@
 # https://www.acmicpc.net/problem/14888
 from itertools import permutations
+import sys
+
 n = int(input())
+op = ['+', '-', '*', '//']
 
-num_arr = list(map(int, input().split()))
-operator = list(map(int, input().split()))
+num_arr = list(input().split())
+op_counts = list(map(int, input().split()))
+operators = []
 
-op = ['+', '-', '*', '%']
-n_a = list(permutations(op))
-cnt = 0
-idx = 0
-max_v = 1e9
-min_v = 0
-for _ in range(len(n_a)):
-    result = num_arr[0] + op[idx] + num_arr[1]
-    for i in range(2, len(num_arr)):
+for x, y in zip(op ,op_counts):
+    for _ in range(y):
+        operators.append(x)
 
 
 
+sqs = set(permutations(operators))
+
+max_value = - sys.maxsize - 1
+min_value = sys.maxsize
+
+for opr in sqs:
+    result = int(num_arr[0])
+    for i in range(len(opr)):
+        if opr[i] == op[3]:
+            temp = result
+            result = eval(str(abs(result)) + op[3] + num_arr[i + 1])
+            if temp < 0:
+                result *= -1
+        else:
+            result = eval(str(result) + opr[i] + num_arr[i+1])
+
+    max_value = max(max_value, result)
+    min_value = min(min_value, result)
+print(max_value)
+print(min_value)
