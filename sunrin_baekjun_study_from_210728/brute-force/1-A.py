0ec56236df51edb4f8e1d5260c191a550916bdcb
xfrnk2 <xfrnk2@gmail.com>
2021-07-31 19:29:06 +0900

    선린 알고리즘 스터디 완전탐색(이지만 재귀/dfs로 푼것) A

--- sunrin_baekjun_study_from_210728/brute-force/1-A.py
+++ sunrin_baekjun_study_from_210728/brute-force/1-A.py
@@ -1,18 +1,34 @@
 # https://www.acmicpc.net/problem/1182
-from itertools import combinations
 
-n, s = list(map(int, input().split()))
-numbers = list(map(int, input().split()))
-c = 0
+# 1차 풀이, combinations 사용
+# from itertools import combinations
+# 
+# n, s = list(map(int, input().split()))
+# numbers = list(map(int, input().split()))
+# c = 0
+# 
+# for i in range(1, n+1):
+#     for j in list(map(sum, combinations(numbers, i))):
+#         if j == s:
+#             c += 1
+# print(c)
 
-for i in range(1, n+1):
-    for j in list(map(sum, combinations(numbers, i))):
-        if j == s:
-            c += 1
+# ----------------------- 더 즣은 방법?
+# 2차 풀이, 재귀 사용..
+def dfs(idx, cur):
+    global c
+    if n <= idx:
+        return
+    cur += numbers[idx]
+    if cur == s:
+        c += 1
+    idx += 1
+    dfs(idx, cur)
+    dfs(idx, cur-numbers[idx-1])
+
+dfs(0, 0)
 print(c)
-
-
-
+# 처음에는 dfs(idx, cur), 그리고 dfs(idx+1, cur)과 같은 형태로 구현해서 틀렸다. 어디서 틀린지 몰라 만지다가 해결됬다.
 
 
 
