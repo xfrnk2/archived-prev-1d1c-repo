5b5e2f40fac96042cf3fb9c2192b68dea41fac3e
xfrnk2attendance <xfrnk2.attendance@gmail.com>
2021-08-13 18:55:59 +0900

    선린 알고리즘 스터디 그리디/스택 B

---
+++ sunrin_baekjun_study_from_210728/greedy_stack/3-B.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/1931
+
+N = int(input())
+c = []
+for _ in range(N):
+    c.append(tuple(map(int, input().split())))
+
+c.sort(key=lambda x:(x[1], x[0]))
+count = 1
+now = c[0][1]
+for i in range(1, len(c)):
+    x, y = c[i]
+    if now <= x:
+        now = y
+        count += 1
+
+print(count)
+
+
+
