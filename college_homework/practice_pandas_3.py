4ca529eb7d44abc9f0975a79e2e668b1ee2ea639
xfrnk2 <xfrnk2@gmail.com>
2021-11-08 23:49:54 +0900

    pandas 연습

---
+++ college_homework/practice_pandas_3.py
@@ -0,0 +1,25 @@
+
+from pandas import read_csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+enqete = read_csv("C:/Users/rad87/Documents/pima2.csv")
+e = enqete
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pregnant_graph = pd.cut(e['pregnant'],  labels=['0-5', '6-10', '10+'], bins=[-1, 5, 10, np.inf])
+
+ff = pd.merge(e.reset_index(), pregnant_graph.reset_index(), on='index')
+result = ff.groupby(["diabetes", "pregnant_y"])
+result = result.agg(
+    {
+        "glucose": ["std", "mean"],
+        "pressure": ["std", "mean"],
+        "triceps": ["std", "mean"],
+        "insulin": ["std", "mean"],
+        "mass":["std", "mean"],
+        "pedigree": ["std", "mean"]
+    }
+)
+print(result)
